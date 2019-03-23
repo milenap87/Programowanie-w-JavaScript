c64fb0acbf00446105fb901593fdb7afffa75807
Milena <milenap87@gmail.com>
2019-03-23 20:11:45 +0100

    kosmetyczne poprawki

--- sekcja-3-zadanie-1/main.js
+++ sekcja-3-zadanie-1/main.js
@@ -1,3 +1,21 @@
+const btn = document.querySelector("button");
+let number = 1;
+btn.addEventListener('click', function () {
+
+    const div = document.createElement('div');
+    div.textContent = number;
+
+    if (number % 5 == 0) {
+        div.classList.add("circle");
+    }
+
+    document.body.appendChild(div);
+    number++;
+});
+
+
+
+
 // function clicked() {
 //     console.log('kliknęto');
 // }; - ta nie działa
@@ -17,19 +35,3 @@
 
 // const btn = document.querySelector("button");
 // btn.addEventListener('click', clicked); - działa
-
-const btn = document.querySelector("button");
-let number = 1; //poza funkcją żeby był dostępny gdy funkcja zakończy działanie i żeby działała iteracja przy każdym kliknięciu
-btn.addEventListener('click', function () {
-    // console.log('jestem kliknięty');
-    const div = document.createElement('div');
-    div.textContent = number;
-
-    // number > 4 && number < 6
-    if (number % 5 == 0) {
-        div.classList.add("circle");
-    }
-
-    document.body.appendChild(div);
-    number++;
-});
